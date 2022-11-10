1940e3d83ee5c47818b8d65110609f552acf903f
reyhancode12 <104006836+reyhancode12@users.noreply.github.com>
2022-11-10 16:28:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+Webcam.set(
+{
+width:350,
+height:300,
+image_format:'png',
+png_quality:90
+
+}
+
+);
+
+
+camera=document.getElementById("camera");
+
+
+Webcam.attach('#camera')
+
+function take_snapshot(){
+
+Webcam.snap(function(data_uri)
+{
+
+document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>'
+
+
+
+}
+);
+}
+
+
+console.log('ml5 version', ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/IAQpcXOdn/",modelLoaded)
+
+function modelLoaded(){
+
+console.log("modelLoaded");
+
+}
+
+function cheack(){
+
+img = document.getElementById('capture_image');
+classifier.classifier(img, gotResult);
+
+}
+
+function gotResult(error,results){
+
+if(error){
+
+console.error(error);
+
+
+}
+
+else{
+
+console.log(results);
+document.getElementById("result_object_accuracy").innerHTML= results[0].label;
+document.getElementById("result_object_accuracy").innerHTML=results[0].confidenec.toFixed(4);
+}
+}
+
+
+
